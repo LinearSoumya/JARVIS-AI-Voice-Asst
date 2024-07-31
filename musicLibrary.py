adcec3f2a76e14b6bd83bcbc451502ddc3caef5e
Soumyadip Roy <130653099+LinearSoumya@users.noreply.github.com>
2024-07-31 19:22:39 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,22 @@
+# musicLibrary.py
+Music = {
+    "sajani": "https://www.youtube.com/watch?v=XJAMVPQWvGY",
+    "zara sa": "https://www.youtube.com/watch?v=5IY4BNj0-10",
+    "labon ko": "https://www.youtube.com/watch?v=-FP2Cmc7zj4",
+    "teri jhuki nazar": "https://www.youtube.com/watch?v=ZgIzch1Pqj4",
+    "nadan parindey": "https://www.youtube.com/watch?v=ttIKsnxPrMY",
+    "ilahi": "https://www.youtube.com/watch?v=fdubeMFwuGs",
+    "let her go": "https://www.youtube.com/watch?v=bL6dJjxm0x0",
+    "mere bina": "https://www.youtube.com/watch?v=XvUSsh3gdO4",
+    "tum se hi": "https://www.youtube.com/watch?v=Cb6wuzOurPc",
+    "main rahoon ya na rahoon": "https://www.youtube.com/watch?v=Dp6lbdoprZ0",
+    "guzarish": "https://www.youtube.com/watch?v=ztPa6vkM-yY",
+    "shayad": "https://www.youtube.com/watch?v=MJyKN-8UncM",
+    "tum hi ho": "https://www.youtube.com/watch?v=NUo8CKI34o4",
+    "tum bin": "https://www.youtube.com/watch?v=gMJFzXZ103Y" , 
+    "hamdard": "https://www.youtube.com/watch?v=FJ55SHCzt88",
+    "kabira": "https://www.youtube.com/watch?v=jHNNMj5bNQw",
+    "tune jo na kaha": "https://www.youtube.com/watch?v=dTu5dTEzVM4",
+    "in dino": "https://www.youtube.com/watch?v=1vZxq-OpVXU",
+    "alvida": "https://www.youtube.com/watch?v=dlBX5YKuanI"
+}
